2f0830162c741e7ee9db92ad5f168c89d1785278
Robert Nubel <rnubel@enova.com>
2013-05-12 17:27:23 -0500

    Mongo-ize some queries.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by_username(params[:username])
+    @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:username], params[:password])
       session[:user_id] = @user.id
       success = true
