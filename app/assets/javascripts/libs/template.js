e52ede16ed44435161043d2bdc4ac7d0d60e05fb
brousalis <brousapg@gmail.com>
2013-05-12 22:46:26 -0500

    Tooltips ahoy

--- app/assets/javascripts/libs/template.js
+++ app/assets/javascripts/libs/template.js
@@ -1,26 +1,57 @@
 nw.template = function() {
   var init = function() {  
+    // the almighty hack of all hacks
+    $('body').tooltip({
+      html: true,
+      animation: false,
+      selector: '[data-toggle=tooltip]'
+    });
+
+    handle_hud();
+  };
+
+  var handle_hud = function() {
+    // shows the popover for specific type
     $('#hud .button')
       .popover({
         placement: 'top',
         html: 'true',
-        animation: false,
-        show: true,
+        animation: 'false',
+        show: 'true',
         content: function() {
           return $('[data-button="'+$(this).attr('rel')+'"]').html();
         }
       })
       .click(function(e) {
         e.preventDefault()
-        $('.button').not(this).popover('hide').removeClass('active');
+        $('.button').not(this).popover('hide')
+                              .removeClass('active');
         $(this).toggleClass('active');
     });  
+    
+    // handles switching the selected power
     $('body').on('click', '.popover .button', function(e) {
-      var style = $(this).find('.image').css('background')
-      $(this).parents().find('.active .image').css('background', style);
-    });
-  };
+      e.preventDefault()
 
+      var power = $(this).attr('rel'),
+          image = $(this).find('.image'),
+          input = $(this).parents().find('.active input'),
+          active = $(this).parents().find('.active .image');
+
+      if(input.val()) {
+        $('[rel="'+input.val()+'"]').removeClass('hidden');
+      }
+       
+      input.val(power);
+
+      $('[rel="'+power+'"]').addClass('hidden');
+
+      active.css('background', image.css('background'));
+
+      $('.button').popover('hide').removeClass('active');
+    }); 
+
+  };
   return {
     init: init
   };
